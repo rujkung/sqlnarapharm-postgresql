059499a9a85bb695f04ea5d1eaeb3d838e593812
eagle_rk <yamaae.f@gmail.com>
2025-09-25 10:03:26 +0700

    Update admit_date_count.sql

--- พี่กรอง/admit_date_count.sql
+++ พี่กรอง/admit_date_count.sql
@@ -1,5 +1,10 @@
-SELECT EXTRACT(MONTH FROM dchdate) AS month,
-       SUM(admdate) AS sum_admdate
-FROM an_stat
-WHERE dchdate >= '2023-05-01' AND dchdate <= '2023-06-30'
-GROUP BY EXTRACT(MONTH FROM dchdate);
+SELECT
+	EXTRACT( MONTH FROM dchdate ) AS MONTH,
+	SUM( admdate ) AS sum_admdate 
+FROM
+	an_stat 
+WHERE
+	dchdate >= '2023-05-01' 
+	AND dchdate <= '2023-06-30' 
+GROUP BY
+	EXTRACT( MONTH FROM dchdate );
